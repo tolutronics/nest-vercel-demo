48930aae67df90e2931009a193c54c0cf0b7c6b9
Tolulope Adeniyi <tolutronics@DJs-MacBook-Pro.local>
2022-10-27 15:46:56 +0100

    init3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/users')
+  getUsers(): any {
+    return this.appService.getUsers();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getUsers(): any {
+    return [
+      {
+        name: 'Tolu',
+        age: 4,
+      },
+      {
+        name: 'Ade',
+        age: 5,
+      },
+    ];
+  }
 }
